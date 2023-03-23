abca75699b37fa5d44bc1a9dd8f6a3ccd07cc60b
gaomingyu <874774259@qq.com>
2023-03-23 16:07:11 +0800

    alpha1

--- make.mjs
+++ make.mjs
@@ -1,5 +1,6 @@
 export default {
     async generate(builder, project, config) {
         const libev = project.createTarget('libev', 'LIBRARY')
+            .addExportIncludeDirectory('.')
     }
 }
